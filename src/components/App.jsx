8c4fa001c38e90274b9de7487191001b96a3f687
favour <kananafavour5@gmail.com>
2026-05-13 13:21:40 +0300

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  // Fetch plants on page load
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  // Filter plants based on search
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+
+      <PlantPage
+        plants={plants}
+        setPlants={setPlants}
+        search={search}
+        setSearch={setSearch}
+        filteredPlants={filteredPlants}
+      />
     </div>
   );
 }
